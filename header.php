91eb7d343844b7e161b26b33c9169ebc285e856a
Maël Schweighardt <mael.schweighardt@epitech.eu>
2014-06-27 17:18:46 +0200

    id="#social_media" added

--- header.php
+++ header.php
@@ -11,6 +11,11 @@
 	<div class="col-lg-12">
 		<!--<h1 id="titre"><a href="index.php" title="Index">Country Click</a></h1>-->
 		<a href="index.php" title="Index"><img src="img/logo.png" /></a>
+		<div id="social_bar">
+			<a href="https://www.facebook.com/countryclick" title="Facebook"><img class="social_element" src="img/facebook.png" alt="facebook" /></a>
+			<a href="#" title=""><img class="social_element" src="img/twitter.png" alt="twitter" /></a>
+			<a href="#" title=""><img class="social_element" src="img/google_plus.png" alt="google_plus" /></a>
+		</div>
 		<?php include("menu.php"); ?>
 	</div>
 </header>
