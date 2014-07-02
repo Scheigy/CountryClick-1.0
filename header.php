7d37036a4ba09db9831a2957fa10c87b526fb39a
Maël Schweighardt <mael.schweighardt@epitech.eu>
2014-07-02 13:59:33 +0200

    Header qui défile

--- header.php
+++ header.php
@@ -17,20 +17,25 @@ session_start();
 		<div class="col-lg-12">
 			<!--<h1 id="titre"><a href="index.php" title="Index">Country Click</a></h1>-->
 			<a href="index.php" title="Index"><img src="img/logo.png" alt="Logo"/></a>
+
 			<div id="social_bar">
-				<a href="https://www.facebook.com/countryclick" target="_blank" title="Facebook"><img class="social_element" src="img/facebook.png" alt="facebook" /></a>
+				<a href="https://www.facebook.com/countryclick" target="_blank" title="Facebook">
+					<img class="social_element" src="img/facebook.png" alt="facebook" />
+				</a>
 				<a href="https://twitter.com/share" target="_blank" data-via="Scheigy" data-lang="fr" data-hashtags="countryclick" title="">
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+					<script>
+						!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+					</script>
 					<img class="social_element" src="img/twitter.png" alt="twitter" />
 				</a>
-
-
-				<a href="#" title=""><img class="social_element" src="img/google_plus.png" alt="google_plus" /></a>
+				<a href="#" title="">
+					<img class="social_element" src="img/google_plus.png" alt="google_plus" />
+				</a>
 			</div>
 			<?php include("menu.php"); ?>
 		</div>
 	</header>
-	<a href="#" title="expand"><img src="img/expand.png" id="exButton" alt="expand" /></a>
+	<div id="exButton"><a href="#" title="expand"><img src="img/expand.png" alt="expand" /></a></div>
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
