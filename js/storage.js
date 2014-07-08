629f26bb6fdcf319bf2eed2c3199932eb33db0c1
SteevenBrunner <steeven.brunner@epitech.eu>
2014-07-08 17:56:28 +0200

    nothing
    nothing

--- js/storage.js
+++ js/storage.js
@@ -1869,4 +1869,13 @@ elemUpgrade_antimatter2.onclick = addUpgrade_antimatter2;
 elemUpgrade_antimatter3 = document.getElementById("twenty-four");
 elemUpgrade_antimatter3.onclick = addUpgrade_antimatter3;
 
+//
+function	check_localStorage()
+{	
+	if (window.localStorage.getItem('money') == NULL)
+		{	
+			
+		}
+}
+//
 sendData(game);
