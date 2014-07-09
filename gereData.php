0a1ce3dadaa8575814d3a1fc416d7e118e83bcab
Maël Schweighardt <mael.schweighardt@epitech.eu>
2014-07-09 15:10:03 +0200

    prompt non

--- gereData.php
+++ gereData.php
@@ -11,8 +11,6 @@ foreach ($_POST as $key => $value)
 	//$line = "UPDATE game (" . $key . ") VALUES (:value) WHERE pseudo = '" . $_SESSION['pseudo'] . "'";
 	$line = "UPDATE game SET " . $key . " = (:value) WHERE pseudo = '" . $_SESSION['pseudo'] . "'";
 
-	echo '______________' . $line . '___________';
-
 	$request = $bdd->prepare($line);
 
 	$request->execute(array('value' => $value));
